99668d355d70c34d120bd27a36fed2a10fcbe8f7
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2023-11-14 17:18:10 +0400

    wip

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,8 +2,13 @@
 
 set -euo pipefail
 
+llvm-objdump --disassemble-symbols=_isatty C:/Windows/System32/ucrtbase.dll
+echo "----------------------"
 cl main.cpp /nologo /EHsc /MD # ucrt
 #https://learn.microsoft.com/en-us/cpp/c-runtime-library/crt-library-features?view=msvc-170
+echo "----------------------"
+file main.exe
+echo "----------------------"
 ./main.exe 
 
 echo "OK"
