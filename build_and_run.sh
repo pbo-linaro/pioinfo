32a5ccc37bfb214a27c4637a4a13ca7f12a2e036
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2023-11-14 16:36:03 +0400

    script to build and run

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-cl main.cpp /MD # ucrt
+cl main.cpp /nologo /MD # ucrt
 #https://learn.microsoft.com/en-us/cpp/c-runtime-library/crt-library-features?view=msvc-170
 ./main.exe 
 
