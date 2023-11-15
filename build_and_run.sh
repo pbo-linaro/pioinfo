dd9617b13a682b2836baaab8058e36a3f94abbce
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2023-11-15 18:35:09 +0400

    wip

--- build_and_run.sh
+++ build_and_run.sh
@@ -7,6 +7,8 @@ echo "----------------------"
 # https://github.com/ajkhoury/pdbfetch
 ./pdbfetch C:/Windows/System32/ucrtbase.dll pdb
 mv pdb/ucrtbase.pdb/*/ucrtbase.pdb .
+echo "Address for pioinfo symbol (from pdb file)"
+llvm-pdbutil pretty ucrtbase.pdb --all | grep pioinfo
 echo "----------------------"
 cl main.cpp /nologo /EHsc /MD # ucrt
 #https://learn.microsoft.com/en-us/cpp/c-runtime-library/crt-library-features?view=msvc-170
