a59cf50ce00fdcb4e97a3adbc4cdae3898d81686
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2023-11-15 18:24:59 +0400

    pdbfetch

--- build_and_run.sh
+++ build_and_run.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 llvm-objdump --disassemble-symbols=_isatty C:/Windows/System32/ucrtbase.dll
 echo "----------------------"
+./pdbfetch C:/Windows/System32/ucrtbase.dll pdb
+mv pdb/ucrtbase.pdb/*/ucrtbase.pdb .
+echo "----------------------"
 cl main.cpp /nologo /EHsc /MD # ucrt
 #https://learn.microsoft.com/en-us/cpp/c-runtime-library/crt-library-features?view=msvc-170
 echo "----------------------"
