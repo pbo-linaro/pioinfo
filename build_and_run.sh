59f1a9b3894211214b6624d5d7eb8a2b71380823
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2023-11-16 10:04:33 +0400

    both debug and non debug

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,19 +3,26 @@
 set -euo pipefail
 
 echo "----------------------"
-cl main.cpp /nologo /EHsc /MD # ucrt
+cl main.cpp /nologo /EHsc /MD /Fe:pioinfo.exe
+cl main.cpp /nologo /EHsc /MDd /Fe:pioinfo_debug.exe
+# /MD, /MDd to target ucrt
 #https://learn.microsoft.com/en-us/cpp/c-runtime-library/crt-library-features?view=msvc-170
 echo "----------------------"
 llvm-objdump --disassemble-symbols=_isatty C:/Windows/System32/ucrtbase.dll
 echo "----------------------"
+llvm-objdump --disassemble-symbols=_isatty C:/Windows/System32/ucrtbased.dll
+echo "----------------------"
 # https://github.com/ajkhoury/pdbfetch
-./pdbfetch C:/Windows/System32/ucrtbase.dll pdb
-mv pdb/ucrtbase.pdb/*/ucrtbase.pdb .
+[ -f ucrtbase.pdb ] || ./pdbfetch C:/Windows/System32/ucrtbase.dll pdb
+[ -f ucrtbased.pdb ] || ./pdbfetch C:/Windows/System32/ucrtbased.dll pdb
+rsync -av pdb/*/*/*.pdb .
 echo "Address for pioinfo symbol (from pdb file)"
 llvm-pdbutil pretty ucrtbase.pdb --all | grep pioinfo
+llvm-pdbutil pretty ucrtbased.pdb --all | grep pioinfo
 echo "----------------------"
-file main.exe
+file pioinfo.exe
+file pioinfo_debug.exe
 echo "----------------------"
-./main.exe 
-
-echo "OK"
+./pioinfo.exe || true
+echo "----------------------"
+./pioinfo_debug.exe || true
