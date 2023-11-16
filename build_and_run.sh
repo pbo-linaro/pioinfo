744b6639c9b54ee327bc765d545011fedf687fe8
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2023-11-16 09:53:24 +0400

    build first

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+echo "----------------------"
+cl main.cpp /nologo /EHsc /MD # ucrt
+#https://learn.microsoft.com/en-us/cpp/c-runtime-library/crt-library-features?view=msvc-170
+echo "----------------------"
 llvm-objdump --disassemble-symbols=_isatty C:/Windows/System32/ucrtbase.dll
 echo "----------------------"
 # https://github.com/ajkhoury/pdbfetch
@@ -9,9 +13,6 @@ echo "----------------------"
 mv pdb/ucrtbase.pdb/*/ucrtbase.pdb .
 echo "Address for pioinfo symbol (from pdb file)"
 llvm-pdbutil pretty ucrtbase.pdb --all | grep pioinfo
-echo "----------------------"
-cl main.cpp /nologo /EHsc /MD # ucrt
-#https://learn.microsoft.com/en-us/cpp/c-runtime-library/crt-library-features?view=msvc-170
 echo "----------------------"
 file main.exe
 echo "----------------------"
